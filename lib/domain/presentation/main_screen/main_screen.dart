47b8dab3ab2cdf54a64c43b5d40b08f56daec230
wogks <wogks27@naver.com>
2023-01-31 13:57:58 +0900

    Feat: main ui

--- lib/domain/presentation/main_screen/main_screen.dart
+++ lib/domain/presentation/main_screen/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:movie_search/domain/presentation/main_screen/main_view_model.dart';
+import 'package:movie_search/domain/presentation/search_screen/search_screen.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -10,8 +11,42 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  final controller = TextEditingController();
+  @override
+  void dispose() {
+    final controller;
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final viewModel = context.watch<MainViewModel>();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.home,
+              size: 25,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SearchScreen()),
+              );
+            },
+            icon: const Icon(
+              Icons.search,
+              size: 25,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
