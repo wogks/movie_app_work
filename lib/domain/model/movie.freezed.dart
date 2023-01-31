750331f9717a39bf6d36a8fbbc8503633b90d2f7
wogks <wogks27@naver.com>
2023-01-31 09:31:06 +0900

    Feat: create model class

---
+++ lib/domain/model/movie.freezed.dart
@@ -0,0 +1,362 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'movie.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Movie _$MovieFromJson(Map<String, dynamic> json) {
+  return _Movie.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Movie {
+  num get id => throw _privateConstructorUsedError;
+  bool get adult => throw _privateConstructorUsedError;
+  @JsonKey(name: 'backdrop_path')
+  String get backdropPath => throw _privateConstructorUsedError;
+  @JsonKey(name: 'original_title')
+  String get originalTitle => throw _privateConstructorUsedError;
+  String get overview => throw _privateConstructorUsedError;
+  num get popularity => throw _privateConstructorUsedError;
+  @JsonKey(name: 'poster_path')
+  String get posterPath => throw _privateConstructorUsedError;
+  @JsonKey(name: 'release_date')
+  String get releaseDate => throw _privateConstructorUsedError;
+  String get title => throw _privateConstructorUsedError;
+  @JsonKey(name: 'vote_average')
+  num get voteAverage => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $MovieCopyWith<Movie> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MovieCopyWith<$Res> {
+  factory $MovieCopyWith(Movie value, $Res Function(Movie) then) =
+      _$MovieCopyWithImpl<$Res, Movie>;
+  @useResult
+  $Res call(
+      {num id,
+      bool adult,
+      @JsonKey(name: 'backdrop_path') String backdropPath,
+      @JsonKey(name: 'original_title') String originalTitle,
+      String overview,
+      num popularity,
+      @JsonKey(name: 'poster_path') String posterPath,
+      @JsonKey(name: 'release_date') String releaseDate,
+      String title,
+      @JsonKey(name: 'vote_average') num voteAverage});
+}
+
+/// @nodoc
+class _$MovieCopyWithImpl<$Res, $Val extends Movie>
+    implements $MovieCopyWith<$Res> {
+  _$MovieCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? adult = null,
+    Object? backdropPath = null,
+    Object? originalTitle = null,
+    Object? overview = null,
+    Object? popularity = null,
+    Object? posterPath = null,
+    Object? releaseDate = null,
+    Object? title = null,
+    Object? voteAverage = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as num,
+      adult: null == adult
+          ? _value.adult
+          : adult // ignore: cast_nullable_to_non_nullable
+              as bool,
+      backdropPath: null == backdropPath
+          ? _value.backdropPath
+          : backdropPath // ignore: cast_nullable_to_non_nullable
+              as String,
+      originalTitle: null == originalTitle
+          ? _value.originalTitle
+          : originalTitle // ignore: cast_nullable_to_non_nullable
+              as String,
+      overview: null == overview
+          ? _value.overview
+          : overview // ignore: cast_nullable_to_non_nullable
+              as String,
+      popularity: null == popularity
+          ? _value.popularity
+          : popularity // ignore: cast_nullable_to_non_nullable
+              as num,
+      posterPath: null == posterPath
+          ? _value.posterPath
+          : posterPath // ignore: cast_nullable_to_non_nullable
+              as String,
+      releaseDate: null == releaseDate
+          ? _value.releaseDate
+          : releaseDate // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      voteAverage: null == voteAverage
+          ? _value.voteAverage
+          : voteAverage // ignore: cast_nullable_to_non_nullable
+              as num,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_MovieCopyWith<$Res> implements $MovieCopyWith<$Res> {
+  factory _$$_MovieCopyWith(_$_Movie value, $Res Function(_$_Movie) then) =
+      __$$_MovieCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {num id,
+      bool adult,
+      @JsonKey(name: 'backdrop_path') String backdropPath,
+      @JsonKey(name: 'original_title') String originalTitle,
+      String overview,
+      num popularity,
+      @JsonKey(name: 'poster_path') String posterPath,
+      @JsonKey(name: 'release_date') String releaseDate,
+      String title,
+      @JsonKey(name: 'vote_average') num voteAverage});
+}
+
+/// @nodoc
+class __$$_MovieCopyWithImpl<$Res> extends _$MovieCopyWithImpl<$Res, _$_Movie>
+    implements _$$_MovieCopyWith<$Res> {
+  __$$_MovieCopyWithImpl(_$_Movie _value, $Res Function(_$_Movie) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? adult = null,
+    Object? backdropPath = null,
+    Object? originalTitle = null,
+    Object? overview = null,
+    Object? popularity = null,
+    Object? posterPath = null,
+    Object? releaseDate = null,
+    Object? title = null,
+    Object? voteAverage = null,
+  }) {
+    return _then(_$_Movie(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as num,
+      adult: null == adult
+          ? _value.adult
+          : adult // ignore: cast_nullable_to_non_nullable
+              as bool,
+      backdropPath: null == backdropPath
+          ? _value.backdropPath
+          : backdropPath // ignore: cast_nullable_to_non_nullable
+              as String,
+      originalTitle: null == originalTitle
+          ? _value.originalTitle
+          : originalTitle // ignore: cast_nullable_to_non_nullable
+              as String,
+      overview: null == overview
+          ? _value.overview
+          : overview // ignore: cast_nullable_to_non_nullable
+              as String,
+      popularity: null == popularity
+          ? _value.popularity
+          : popularity // ignore: cast_nullable_to_non_nullable
+              as num,
+      posterPath: null == posterPath
+          ? _value.posterPath
+          : posterPath // ignore: cast_nullable_to_non_nullable
+              as String,
+      releaseDate: null == releaseDate
+          ? _value.releaseDate
+          : releaseDate // ignore: cast_nullable_to_non_nullable
+              as String,
+      title: null == title
+          ? _value.title
+          : title // ignore: cast_nullable_to_non_nullable
+              as String,
+      voteAverage: null == voteAverage
+          ? _value.voteAverage
+          : voteAverage // ignore: cast_nullable_to_non_nullable
+              as num,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_Movie implements _Movie {
+  const _$_Movie(
+      {required this.id,
+      required this.adult,
+      @JsonKey(name: 'backdrop_path') required this.backdropPath,
+      @JsonKey(name: 'original_title') required this.originalTitle,
+      required this.overview,
+      required this.popularity,
+      @JsonKey(name: 'poster_path') required this.posterPath,
+      @JsonKey(name: 'release_date') required this.releaseDate,
+      required this.title,
+      @JsonKey(name: 'vote_average') required this.voteAverage});
+
+  factory _$_Movie.fromJson(Map<String, dynamic> json) =>
+      _$$_MovieFromJson(json);
+
+  @override
+  final num id;
+  @override
+  final bool adult;
+  @override
+  @JsonKey(name: 'backdrop_path')
+  final String backdropPath;
+  @override
+  @JsonKey(name: 'original_title')
+  final String originalTitle;
+  @override
+  final String overview;
+  @override
+  final num popularity;
+  @override
+  @JsonKey(name: 'poster_path')
+  final String posterPath;
+  @override
+  @JsonKey(name: 'release_date')
+  final String releaseDate;
+  @override
+  final String title;
+  @override
+  @JsonKey(name: 'vote_average')
+  final num voteAverage;
+
+  @override
+  String toString() {
+    return 'Movie(id: $id, adult: $adult, backdropPath: $backdropPath, originalTitle: $originalTitle, overview: $overview, popularity: $popularity, posterPath: $posterPath, releaseDate: $releaseDate, title: $title, voteAverage: $voteAverage)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_Movie &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.adult, adult) || other.adult == adult) &&
+            (identical(other.backdropPath, backdropPath) ||
+                other.backdropPath == backdropPath) &&
+            (identical(other.originalTitle, originalTitle) ||
+                other.originalTitle == originalTitle) &&
+            (identical(other.overview, overview) ||
+                other.overview == overview) &&
+            (identical(other.popularity, popularity) ||
+                other.popularity == popularity) &&
+            (identical(other.posterPath, posterPath) ||
+                other.posterPath == posterPath) &&
+            (identical(other.releaseDate, releaseDate) ||
+                other.releaseDate == releaseDate) &&
+            (identical(other.title, title) || other.title == title) &&
+            (identical(other.voteAverage, voteAverage) ||
+                other.voteAverage == voteAverage));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      id,
+      adult,
+      backdropPath,
+      originalTitle,
+      overview,
+      popularity,
+      posterPath,
+      releaseDate,
+      title,
+      voteAverage);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_MovieCopyWith<_$_Movie> get copyWith =>
+      __$$_MovieCopyWithImpl<_$_Movie>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_MovieToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Movie implements Movie {
+  const factory _Movie(
+          {required final num id,
+          required final bool adult,
+          @JsonKey(name: 'backdrop_path') required final String backdropPath,
+          @JsonKey(name: 'original_title') required final String originalTitle,
+          required final String overview,
+          required final num popularity,
+          @JsonKey(name: 'poster_path') required final String posterPath,
+          @JsonKey(name: 'release_date') required final String releaseDate,
+          required final String title,
+          @JsonKey(name: 'vote_average') required final num voteAverage}) =
+      _$_Movie;
+
+  factory _Movie.fromJson(Map<String, dynamic> json) = _$_Movie.fromJson;
+
+  @override
+  num get id;
+  @override
+  bool get adult;
+  @override
+  @JsonKey(name: 'backdrop_path')
+  String get backdropPath;
+  @override
+  @JsonKey(name: 'original_title')
+  String get originalTitle;
+  @override
+  String get overview;
+  @override
+  num get popularity;
+  @override
+  @JsonKey(name: 'poster_path')
+  String get posterPath;
+  @override
+  @JsonKey(name: 'release_date')
+  String get releaseDate;
+  @override
+  String get title;
+  @override
+  @JsonKey(name: 'vote_average')
+  num get voteAverage;
+  @override
+  @JsonKey(ignore: true)
+  _$$_MovieCopyWith<_$_Movie> get copyWith =>
+      throw _privateConstructorUsedError;
+}
